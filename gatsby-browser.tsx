7ede61bd9e1baa03b7ee766f481b674271809897
RobertGemmaJr <rgemma.jr@gmail.com>
2023-01-15 19:48:53 -0500

    chore: Add TODO

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,13 +1,16 @@
-import * as React from "react"
-import type { GatsbyBrowser } from "gatsby"
+import * as React from "react";
+import type { GatsbyBrowser } from "gatsby";
+import { ChakraProvider, Heading } from "@chakra-ui/react";
+
+import theme from "@src/theme";
 
 export const wrapPageElement: GatsbyBrowser["wrapPageElement"] = ({
   element,
 }) => {
   return (
-    <div>
-      <h1>Wrap Page Element</h1>
+    <ChakraProvider theme={theme} resetCSS>
+      <Heading>Hello, World!</Heading>
       {element}
-    </div>
-  )
-}
+    </ChakraProvider>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 
+// Add linter
+// Rest of chakra stuff?
+
+// See chakra example website for SEO and other nice components
+// https://github.com/chakra-ui/chakra-ui/tree/e98db2cc02e530c980997353dd4f093da9064593/examples/gatsby
+
 export default function IndexPage(props: PageProps) {
   console.log(props)
   return (
