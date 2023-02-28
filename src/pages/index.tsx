ae3453ffff00f0eb8d0abcba2de8547ea36a2c14
RobertGemmaJr <rgemma.jr@gmail.com>
2023-02-28 15:46:35 -0500

    feat: TS for index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import type { PageProps } from 'gatsby'
 import { Heading } from '@chakra-ui/react'
 
-const IndexRoute = ({ path }: PageProps) => {
+const IndexRoute: React.FC<PageProps> = ({ path }) => {
   return (
     <main>
       <Heading size='sm'>Path: {path}</Heading>
