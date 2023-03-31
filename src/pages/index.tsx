5b182fe88d319937ce36802a2a841ea521a2dd91
RobertGemmaJr <rgemma.jr@gmail.com>
2023-03-31 12:48:24 -0400

    chore: commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import type { PageProps } from 'gatsby';
 import { Heading } from '@chakra-ui/react';
 
 const IndexRoute: React.FC<PageProps> = ({ path }) => {
+  //comment
   return (
     <main>
       <Heading size='sm'>Path: {path}</Heading>
