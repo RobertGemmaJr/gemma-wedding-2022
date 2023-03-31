5cc61cb274f500960da1922e4f90e604e87f069e
RobertGemmaJr <rgemma.jr@gmail.com>
2023-03-31 12:45:06 -0400

    chore: test precommit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import { Heading } from '@chakra-ui/react';
 const IndexRoute: React.FC<PageProps> = ({ path }) => {
   return (
     <main>
+
+      
       <Heading size="sm">Path: {path}</Heading>
     </main>
   );
