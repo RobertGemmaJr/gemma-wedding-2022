b8267c52af8734656793932862d73500f4695704
RobertGemmaJr <robert_gemma@brown.edu>
2023-11-14 16:39:37 -0500

    ref: Use chakra components for vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,49 @@
 import { useState } from "react";
+
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import { Box, Heading, Link, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/image";
+import { Button } from "@chakra-ui/button";
 
-// TODO: chakra, prettier, husky
+// TODO: chakra, prettier, husky, @ imports
+// TODO: Color mode
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
+      <Box>
+        <Link
+          href="https://vitejs.dev"
+          target="_blank"
+        >
+          <Image
+            src={viteLogo}
+            className="logo"
+            alt="Vite logo"
+          />
+        </Link>
+        <Link
+          href="https://react.dev"
+          target="_blank"
+        >
+          <Image
+            src={reactLogo}
+            className="logo react"
+            alt="React logo"
+          />
+        </Link>
+      </Box>
+      <Heading>Vite + React</Heading>
+      <Box className="card">
+        <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
+        <Text>
           Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        </Text>
+      </Box>
+      <Text className="read-the-docs">Click on the Vite and React logos to learn more</Text>
     </>
   );
 }
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./theme";
 import App from "./App.tsx";
 
-const rootElement = document.getElementById("root")!;
-ReactDOM.createRoot(rootElement).render(
+const ROOT_ELEMENT = document.getElementById("root")!;
+ReactDOM.createRoot(ROOT_ELEMENT).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <App />
