57581d3774eeb319b8bcf33ec0c3c0fad3a8f6fc
RobertGemmaJr <robert_gemma@brown.edu>
2023-11-22 13:01:57 -0500

    feat: Init firebase project

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,4 +2,10 @@ module.exports = {
   printWidth: 100,
   quoteProps: "as-needed",
   trailingComma: "es5",
+  overrides: [
+    {
+      files: ".firebaserc",
+      options: { parser: "json" },
+    },
+  ],
 };
